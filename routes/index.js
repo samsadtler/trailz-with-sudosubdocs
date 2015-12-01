28442747748752e09e1b4766593acb4fb52b56c0
Sam Sadtler <sam.sadtler@gmail.com>
2015-12-01 13:23:08 -0500

    console.logging shit

--- routes/index.js
+++ routes/index.js
@@ -95,7 +95,7 @@ router.post('/api/create/trail', function(req,res){
 
 router.post('/api/add/bookmarks', function(req,res){
     var bookmarks = req.body.tree;
-    console.log("req -->"+ JSON.stringify(req.body))
+    console.log("req.body -->"+ JSON.stringify(req.body))
     console.log("flatChildrenArray " + depthFirst(bookmarks, 0))
 
     function depthFirst(tree, depth){
